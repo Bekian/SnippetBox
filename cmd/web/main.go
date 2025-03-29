a5cdd1d7e40969d2132681ae9d77b30a37eba0fe
Bekian <Bekian.Cat@gmail.com>
2025-03-29 11:11:09 -0500

    feat(app): finished up to 3.2, added basic structured logging
    see changes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+	/// config
+	// addr port string uses the 4000 port by default
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	flag.Parse()
+	// logger init
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// router
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
+	// serve static files
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", home)
@@ -16,8 +26,9 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4000")
-
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	// server
+	logger.Info("starting server", "addr", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
