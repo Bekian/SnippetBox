96b3a6da3099eb15553c2499f1cb18f07bbb6a1e
Bekian <Bekian.Cat@gmail.com>
2025-03-29 21:02:01 -0500

    feat(app): added sql init calls to actually run mysql
    see changes, not much here

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,9 +18,18 @@ func main() {
 	/// config
 	// addr port string uses the 4000 port by default
 	addr := flag.String("addr", ":8080", "HTTP network address")
+	// dsn string for mysql connection
+	dsn := flag.String("dsn", "web:1234@/snippetbox?parseTime=true", "MySQL data source name string")
 	flag.Parse()
 	// logger init
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
@@ -25,7 +37,22 @@ func main() {
 
 	// server
 	logger.Info("starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
