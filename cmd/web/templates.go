331f1f52c4323f6002dd2e80c05518c6c92b0c23
Bekian <Bekian.Cat@gmail.com>
2025-04-04 21:32:28 -0500

    feat(app): added template caching and helper
    added a template cache that is a map to hold loaded templates, also added a render helper function to abstract template loading for specific views

---
+++ cmd/web/templates.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/Bekian/SnippetBox/internal/models"
+	"html/template"
+	"path/filepath"
+)
+
+// dep injection struct for holding template data
+type templateData struct {
+	Snippet  models.Snippet
+	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// map to act as the cache
+	cache := map[string]*template.Template{}
+	// grab all files that match the following pattern
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		// get the file's base name
+		name := filepath.Base(page)
+
+		// parse base template into a template set
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// this loads the partials onto the base template set
+		ts, err = ts.ParseGlob("./ui/html/templates/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// add the page to the template set
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// add template set to the cache map
+		// with the base name as the key
+		cache[name] = ts
+	}
+
+	return cache, nil
+}
