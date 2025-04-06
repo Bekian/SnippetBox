31e450e853a928a99c0c73312dc93e19290dfb19
Bekian <Bekian.Cat@gmail.com>
2025-04-05 21:04:58 -0500

    feat(app): finished up to 6.3, request logging
    added request logging, and common header setting for basic routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,17 +2,17 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	// router
 	mux := http.NewServeMux()
 	// serve static files
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
+	// server app routes
 	mux.HandleFunc("GET /{$}", app.home)
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	return mux
+	return app.logRequest(commonHeaders(mux))
 }
